837a413cd9452fe726f4e3699ba27119de4259b8
dheeraj <dheerajmahra873@gmail.com>
2020-09-03 22:48:16 +0530

    prettier config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      Hi
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>hi</h1>
+		</div>
+	);
 }
 
 export default App;
